f9f7191d7fca6fe4c7d5b2f60035bb320d28bef6
Joseadrian <joseadrian@outlook.com>
2014-07-07 14:08:39 -0500

    Add package.json && update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    connect = require('gulp-connect');
 
 gulp.task('imgur', function() {
 
@@ -12,11 +13,26 @@ gulp.task('vine', function() {
 gulp.task('upload', ['imgur', 'vine']);
 
 gulp.task('less', function() {
-  gulp.src('./assets/css/app.less')
+  gulp.src(['./assets/css/app.less'])
       .pipe(less())
-      .pipe(gulp.dest('./assets/css'));
+      .pipe(gulp.dest('./assets/css'))
+      .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
+gulp.task('js', function() {
+  gulp.src('./assets/js/*.js')
+      .pipe(connect.reload())
+});
+
+gulp.task('server', function() {
+  connect.server({
+    root: './',
+    port: 8080,
+    livereload: true
+  })
+});
+
+gulp.task('default', ['server'], function() {
   gulp.watch('./assets/css/app.less', ['less']);
+  gulp.watch('./assets/js/*.js', ['js']);
 });
